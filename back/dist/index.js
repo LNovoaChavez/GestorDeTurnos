6fcfcbe208b0538cb62d8e4be64a22f43e094378
Luz NovCh <luznov06@gmail.com>
2024-04-26 19:20:57 -0300

    Lecture 2 realizadaa

---
+++ back/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const num1 = 5;
+const num2 = 10;
+const sumar = (a, b) => a + b;
+console.log(sumar(num1, num2));
---
+++ back/index.ts
@@ -0,0 +1,7 @@
+const num1: number = 5
+const num2: number  = 10
+
+const sumar = (a: number, b: number) => a + b
+
+console.log(sumar(num1, num2))
+
